ed45199f3df6a5ae7eeca03b9e70138f0f7552fa
m.yousefi <m.yousefi@titan-diesel.com>
2022-07-19 12:18:32 +0430

    Add Sample

--- ValidationHelper/Program.cs
+++ ValidationHelper/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace ValidationHelper
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ValidationHelper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var typeOfClass = typeof(Validations)
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly)
+                .ToList();
+
+
+            var menu = new EasyConsole.Menu();
+            foreach (var item in typeOfClass)
+            {
+                menu.Add(item.Name, () =>
+                {
+                    var res = item.GetValue(null);
+                    if (res == null)
+                        return;
+
+                    Console.WriteLine("Enter " + item.Name + " To Validate:");
+
+                    var userData = Console.ReadLine();
+                    if (string.IsNullOrEmpty(userData))
+                    {
+                        Console.WriteLine("Invalid input");
+                        return;
+                    }
+
+                    Console.WriteLine("------------------");
+                    Console.WriteLine("Validate Result:" + Regex.IsMatch(userData, (string)res));
+                    Console.WriteLine("------------------");
+
+                });
+            }
+
+            menu.Add("Nationalcode", () =>
+            {
+                Console.WriteLine("Enter national code To validate:");
+                var userData = Console.ReadLine();
+                if (string.IsNullOrEmpty(userData))
+                {
+                    Console.WriteLine("Invalid input");
+                    return;
+                }
+
+                Console.WriteLine("------------------");
+                Console.WriteLine("Validate Result:" + Validations.IsNationalCode(userData));
+                Console.WriteLine("------------------");
+            });
+
+            menu.Display();
         }
     }
 }
